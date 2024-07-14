4911a5a971697b4d7ade02a4ab8e1fee09c6c1ef
Andkorz <opttorgportal@gmail.com>
2024-07-14 15:15:38 +0200

    added data.py, elements_page.py, elements_page_locators.py, elements_test.py, generator.py

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,11 +1,14 @@
-from pages.base_page import BasePage
-import time
+
+from pages.elements_page import TextBoxPage
 
 
-def test_example(driver):
-    page = BasePage(driver, "google.com")
-    page.open()
-    time.sleep(3)
+class TestElements:
+    class TestTextBox:
 
-
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
+            text_box_page.open()
+            input_data = text_box_page.fill_all_fields()
+            output_data = text_box_page.check_filled_form()
+            assert input_data == output_data
 
