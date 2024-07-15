ad5306326c4e1fdfff1b02cc0751e2870044b435
Andkorz <opttorgportal@gmail.com>
2024-07-15 14:13:10 +0200

    fixed go_to_element in BasePage updated elements tests added test checkboxes

--- pages/base_page.py
+++ pages/base_page.py
@@ -28,6 +28,6 @@ class BasePage:
 
 
     def go_to_element(self, element):
-        self.driver.execute_script("argument[0].scrollIntoView();", element)
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
 
 
